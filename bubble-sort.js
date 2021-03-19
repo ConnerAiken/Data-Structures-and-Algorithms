d557219c6d727b225c25e666b9eebbea3ab0d537
Conner Aiken <conner@fittedtech.com>
2021-03-18 21:44:18 -0700

    init to win it

---
+++ bubble-sort.js
@@ -0,0 +1,26 @@
+var unsortedArray = [
+    1, 9, 5, 7, 3, 5, 1, 9, 8, 2, 5, 4
+];
+ 
+bubbleSort(unsortedArray);
+
+
+function bubbleSort(sourceArr) {
+    let tmp1, tmp2;
+    var returnArr = [];
+    
+    console.log("Starting Bubble Sort - source array: "+sourceArr.join(','));
+    
+    sourceArr.forEach((item, index) => {
+        if(index >= 1) { 
+        }
+
+        console.log(''); 
+        
+        if(item[i] > item[i+1]) { 
+            item[i+1] = item[i];
+            item[i] = tmp1; 
+        } 
+    });
+}
+  
